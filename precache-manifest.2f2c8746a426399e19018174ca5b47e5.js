17bbac7f0e06f9ac52278a09657a315530f35f8b
karan-10001 <karanpratapsingh10001@gmail.com>
2023-04-03 18:01:01 +0530

    Updates

---
+++ precache-manifest.2f2c8746a426399e19018174ca5b47e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f099a97490e44a75c79",
+    "url": "/textUtilityApplication/static/css/main.92b2c8e6.chunk.css"
+  },
+  {
+    "revision": "0f099a97490e44a75c79",
+    "url": "/textUtilityApplication/static/js/main.0f099a97.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/textUtilityApplication/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "9f6d8a0aec7ef54759d1",
+    "url": "/textUtilityApplication/static/js/2.9f6d8a0a.chunk.js"
+  },
+  {
+    "revision": "8ab299d7657c9081f3b8",
+    "url": "/textUtilityApplication/static/js/runtime~main.8ab299d7.js"
+  },
+  {
+    "revision": "7b966aa590a86d8a607930875b1bb0c8",
+    "url": "/textUtilityApplication/index.html"
+  }
+];
